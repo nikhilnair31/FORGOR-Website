4e2ea3190b78a9a70e51e0262b5b3e05935ed711
Nikhil Nair <niknair31898@gmail.com>
2025-05-31 17:38:59 -0400

    Tweaked user and app

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,18 @@ export default {
             // Define your custom color palette
             colors: {
                 purple: { // Use a nested structure for shades if you have them
+                    200: '#af94de',
                     DEFAULT: '#1e1c2c', // Your primary purple
                     800: '#221f31',
                 },
-                neonGreen: '#deff96', // Your exact neon green color
-                brightPurple: '#7654b1', // Your exact bright purple color
+                neonGreen: {
+                    200: '#d4ff75',
+                    DEFAULT: '#deff96',
+                },
+                brightPurple: {
+                    200: '#deccff',
+                    DEFAULT: '#7654b1',
+                },
             },
             borderRadius: {
               'none': '0', // Ensures 'rounded-none' maps to 0
