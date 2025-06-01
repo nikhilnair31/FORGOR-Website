782ee757ee1fdb31fe12dbb4ac1a682926369e16
Nikhil Nair <niknair31898@gmail.com>
2025-05-31 20:45:49 -0400

    more tweaks

--- src/js/authUtils.js
+++ src/js/authUtils.js
@@ -1,7 +1,14 @@
 // src/js/authUtils.js
 export function checkAuthAndRedirect() {
+    // Ensure this function only runs in the browser environment
+    if (typeof window === 'undefined') {
+        return; // Don't execute on the server
+    }
+    
     const accessToken = localStorage.getItem('access_token'); // Use 'access_token' as per your login.js
+    console.log(`accessToken: ${accessToken}`);
     const currentPage = window.location.pathname;
+    console.log(`currentPage: ${currentPage}`);
 
     const protectedPages = [
         '/app',
